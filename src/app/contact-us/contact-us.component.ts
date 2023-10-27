96c6788085bcd18253a7a138ddaaf5e09221c9d1
Bhavana <bhavana@oaks.guru>
2023-10-27 20:49:27 +0530

    enquiry btn

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -8,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class ContactUsComponent implements OnInit {
 
   constructor() { }
+  contact_details = [
+    {
+      "icon": "call",
+      "title": "Phone",
+      "desc": "8885011455"
+    },
+    {
+      "icon": "mail",
+      "title": "Email",
+      "desc": "thegifthamper23@gmail.com"
+    },
+    {
+      "icon": "access_time",
+      "title": "Working Hours",
+      "desc": "Monday - Saturday : 8am - 22pm"
+    },
+    {
+      "icon": "location_on",
+      "title": "Address",
+      "desc": "303, Pavani Aqua Apartment, Masjid Banda, Kondapur 500084"
+    }
+  ]
 
   ngOnInit(): void {
   }
-  Submit(ev: any){
+  Submit(ev: any) {
     console.log(ev, "data");
   }
 }
